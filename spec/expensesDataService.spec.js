4e9dcf37f5111b272fcf305172c45b35bcbf2ea5
Chi Bui <Chi_bui@outlook.com>
2018-05-10 16:41:33 +1000

    refactor

--- spec/expensesDataService.spec.js
+++ spec/expensesDataService.spec.js
@@ -5,4 +5,18 @@ describe('expensesDataService', function () {
     it('should return three expense items', inject(function (expensesDataService) {
          expect(expensesDataService.getExpenses().length).toBe(3);
     }));
+
+    it('should return a taxi expense ', inject(function (expensesDataService) {
+        var expenseItems = expensesDataService.getExpenses(),
+            testExpenseItem = new ExpenseItem('Taxi', 'To airport', 89.95);
+
+        expect(expenseItems).toContain(testExpenseItem);
+    }));
+
+    it('taxi should be a reasonable expense', function () {
+        var taxi = new ExpenseItem('Taxi', 'To airport', 89.95);
+
+        expect(taxi).toBeLessThan(100);
+    });
+
 });
--- src/app/expense/expenseItem.js
+++ src/app/expense/expenseItem.js
@@ -1,3 +1,5 @@
-function ExpenseItem(amount) {
+function ExpenseItem(type, description, amount) {
+    this.type = type;
+    this.description = description;
     this.amount = amount;
 }
